190606f74e990b469feb8cc4a492667d5d8baaff
Omer Munk <omermunk95@gmail.com>
2024-11-26 12:31:53 +0700

    first task - create and get transaction

--- services/transaction_service/app.py
+++ services/transaction_service/app.py
@@ -2,6 +2,31 @@ from flask import Flask, jsonify
 from neo4j import GraphDatabase
 import redis
 
+
+
+
+"""
+how does the transaction node look?
+מי שילם
+למי היא שילם
+כמה הוא שילם
+תאריך של התשלום
+מטבע
+
+source_id
+target_id
+transaction_id
+amount
+timestamp
+currency
+
+
+how does the account node looks?
+account_id
+
+
+"""
+
 app = Flask(__name__)
 
 
@@ -16,10 +41,38 @@ redis_client = redis.Redis(
     decode_responses=True
 )
 
+class TransactionRepository:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def create_transaction(self, transaction_data):
+        # TODO: create transaction node in neo4j db
+        # todo: create a session
+        # todo: create a cipher query
+        # todo: run with the session, the query
+        # todo: return the transaction that was inserted
+        pass
+
+    def get_transaction(self, transaction_id):
+        #TODO: create a session
+        #todo: create a cipher query
+        #todo: run with the session, the query
+        #todo: return the transaction that was found
+        pass
+
 @app.route('/api/v1/transaction', methods=['POST'])
 def create_transaction():
     return jsonify({"message": "Transaction created"}), 200
 
+
+@app.route('/api/v1/transaction/<transaction_id>', methods=['GET'])
+def get_transaction():
+    return jsonify({"message": "Transaction recieved"}), 200
+
+
+@app.route('/api/v1/transaction/search', methods=['GET'])
+def search_transactions():
+    return jsonify({"message": "Transaction searched"}), 200
 
 
 
